a660013d3f7bdb7f13a69b9fb52a629275949b43
David Jimenez <dajimenezja@gmail.com>
2018-01-17 16:53:25 -0700

    partial implementation

--- app/src/main/java/com/example/aidlservice/AidlService.java
+++ app/src/main/java/com/example/aidlservice/AidlService.java
@@ -4,15 +4,13 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.os.RemoteException;
-import android.support.annotation.Nullable;
 import android.util.Log;
 
 public class AidlService extends Service {
 
-
-    @Nullable
     @Override
     public IBinder onBind(Intent intent) {
+        Log.d("binderOnBind", "onBind: trying to bind...");
         return new IAidlService.Stub() {
             @Override
             public void sendCommand(String command) throws RemoteException {
